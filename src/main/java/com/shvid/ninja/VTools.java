a42fe11a886b7a7b11056d4c9a61b80ab85e9c8c
ashvid <ashvid@D-SJN-00547026.corp.ebay.com>
2013-10-03 16:44:48 -0700

    vTools grow

--- src/main/java/com/shvid/ninja/VTools.java
+++ src/main/java/com/shvid/ninja/VTools.java
@@ -4,8 +4,96 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
+/**
+ * VTools Helper class
+ * 
+ * This class implements logic for fast serialization/deserialization of the variable data types
+ * 
+ * 
+ * @author Alex Shvid
+ *
+ */
+
+
 public final class VTools {
 
+	/**
+	 * Serialize Long to VLong form
+	 * 
+	 * [d][d][d][d][d][s][N][n] [d][d][d][d][d][d][d][n] [d][d][d][d][d][d][d][n] [d][d][d][d][d][d][d][n] ...
+	 * 
+	 * where:
+	 * d - data bit
+	 * s - sign bit
+	 * n - next byte
+	 * N - is null
+	 * 
+	 * @param out - serialization stream
+	 * @param lvalue
+	 * @throws IOException
+	 */
+	
+	public static final void writeVLongObject(DataOutput out, Long lvalueRef) throws IOException {
+		if (lvalueRef == null) {
+			out.writeByte(0x40);
+			return;
+		}
+		long lvalue = lvalueRef.longValue();
+		int unsignedByte = 0;
+		if (lvalue < 0L) {
+			unsignedByte = 0x20; lvalue ^= -1L; 
+		}
+	    unsignedByte |= (byte) ((int) lvalue & 0x1F);
+	    lvalue >>>= 5;
+	    while(lvalue != 0L) {
+	    	unsignedByte |= 0x80;
+	    	out.writeByte(unsignedByte);
+	    	unsignedByte = (int) lvalue & 0x7F;
+	    	lvalue >>>= 7;
+	    }
+	    out.writeByte(unsignedByte);
+	}	
+	
+	/**
+	 * Deserialize VLong to Long
+	 * 
+	 * @param in - deserialization stream
+	 * @return long
+	 * @throws IOException
+	 */
+	
+	public static final Long readVLongObject(DataInput in) throws IOException {
+		int unsignedByte = in.readUnsignedByte();
+		if ((unsignedByte & 0x40) != 0) {
+			return null;
+		}
+		long lvalue = unsignedByte & 0x1F;
+		int numBits = 5;
+		boolean isNegative = (unsignedByte & 0x20) != 0;
+		while((unsignedByte & 0x80) != 0) {
+			unsignedByte = in.readUnsignedByte();
+			lvalue |= (long)(unsignedByte & 0x7F) << numBits;
+			numBits += 7;
+		}
+		return isNegative ? lvalue ^ -1L : lvalue;
+	}
+
+	
+	/**
+	 * Serialize long in VLong form
+	 * 
+	 * [d][d][d][d][d][d][s][n] [d][d][d][d][d][d][d][n] [d][d][d][d][d][d][d][n] [d][d][d][d][d][d][d][n] ...
+	 * 
+	 * where:
+	 * d - data bit
+	 * s - sign bit
+	 * n - next byte
+	 * 
+	 * @param out - serialization stream
+	 * @param lvalue
+	 * @throws IOException
+	 */
+	
 	public static final void writeVLong(DataOutput out, long lvalue) throws IOException {
 		int unsignedByte = 0;
 		if (lvalue < 0L) {
@@ -21,6 +109,14 @@ public final class VTools {
 	    }
 	    out.writeByte(unsignedByte);
 	}
+	
+	/**
+	 * Deserialize VLong to long
+	 * 
+	 * @param in - deserialization stream
+	 * @return long
+	 * @throws IOException
+	 */
 	
 	public static final long readVLong(DataInput in) throws IOException {
 		int unsignedByte = in.readUnsignedByte();
--- src/test/java/com/shvid/ninja/VToolsTest.java
+++ src/test/java/com/shvid/ninja/VToolsTest.java
@@ -31,5 +31,30 @@ public class VToolsTest extends TestCase {
 		long actualValue = VTools.readVLong(new DataInputStream(new ByteArrayInputStream(serializedVLong)));
 		return actualValue;
 	}
+
+	
+	public void testVLongObject() throws IOException {
+		assertEquals(null, serializeAndDeserializeObject(null));
+		assertEquals(new Long(0L), serializeAndDeserializeObject(0L));
+		assertEquals(new Long(123L), serializeAndDeserializeObject(123L));
+		assertEquals(new Long(-123L), serializeAndDeserializeObject(-123L));
+		assertEquals(new Long(Integer.MAX_VALUE), serializeAndDeserializeObject(new Long(Integer.MAX_VALUE)));
+		assertEquals(new Long(Integer.MIN_VALUE), serializeAndDeserializeObject(new Long(Integer.MIN_VALUE)));
+		assertEquals(new Long(Long.MAX_VALUE), serializeAndDeserializeObject(Long.MAX_VALUE));
+		assertEquals(new Long(Long.MIN_VALUE), serializeAndDeserializeObject(Long.MIN_VALUE));
+	}
+	
+	private Long serializeAndDeserializeObject(Long expectedValue) throws IOException {
+		ByteArrayOutputStream bout = new ByteArrayOutputStream();
+		DataOutputStream dout = new DataOutputStream(bout);
+		VTools.writeVLongObject(dout, expectedValue);
+		
+		byte[] serializedVLong = bout.toByteArray();
+		//System.out.println("VLong " + Arrays.toString(serializedVLong));
+		
+		Long actualValue = VTools.readVLongObject(new DataInputStream(new ByteArrayInputStream(serializedVLong)));
+		return actualValue;
+	}
+	
 	
 }
